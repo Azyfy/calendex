5c926a42f207a81ca1f46a2fb7dc451d9c7403e4
Azryel <AzryelShroud@gmail.com>
2022-05-11 23:07:39 +0200

    The event type selection page

--- lib/calendex_web/live/page_live.ex
+++ lib/calendex_web/live/page_live.ex
@@ -1,8 +1,15 @@
 defmodule CalendexWeb.PageLive do
   use CalendexWeb, :live_view
 
+  # We will implement this module in a minute...
+  alias CalendexWeb.Components.EventType
+
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    event_types = Calendex.available_event_types()
+
+    {:ok, assign(socket, event_types: event_types),
+            temporary_assigns: [event_types: []]
+    }
   end
 end
