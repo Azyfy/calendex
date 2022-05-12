b82dc1cceabc781c6291a851bcbf74a202d7b365
Azryel <AzryelShroud@gmail.com>
2022-05-12 21:00:06 +0200

    Listing event types

--- lib/calendex_web/live/admin/event_types_live.ex
+++ lib/calendex_web/live/admin/event_types_live.ex
@@ -8,10 +8,14 @@ defmodule CalendexWeb.Admin.EventTypesLive do
 
   @impl LiveView
   def handle_params(_, _, socket) do
+    event_types = Calendex.available_event_types()
+
     socket =
       socket
       |> assign(section: "event_types")
       |> assign(page_title: "Event types")
+      |> assign(event_types: event_types)
+      |> assign(event_types_count: length(event_types))
 
     {:noreply, socket}
   end
