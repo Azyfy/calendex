9431e2f01508847220d45d026558cd5a14839010
Azryel <AzryelShroud@gmail.com>
2022-05-16 21:00:27 +0200

    The event type dropdown menu

--- lib/calendex_web/live/admin/components/event_type.ex
+++ lib/calendex_web/live/admin/components/event_type.ex
@@ -1,6 +1,8 @@
 defmodule CalendexWeb.Admin.Components.EventType do
   use CalendexWeb, :live_component
 
+  alias CalendexWeb.Admin.Components.Dropdown
+
   def mount(socket) do
     {:ok, socket}
   end
